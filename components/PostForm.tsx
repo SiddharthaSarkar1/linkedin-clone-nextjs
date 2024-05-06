5875c6be26fd3dafe77c280c49d17352867254c6
Siddhartha Sarkar <mail.04.siddhartha@gmail.com>
2024-05-07 01:43:33 +0530

    add db schemas

--- components/PostForm.tsx
+++ components/PostForm.tsx
@@ -1,16 +1,54 @@
 "use client";
 import { useUser } from "@clerk/nextjs";
-import React from "react";
+import React, { useRef, useState } from "react";
 import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
 import { Button } from "./ui/button";
-import { ImageIcon } from "lucide-react";
+import { ImageIcon, XIcon } from "lucide-react";
 
 const PostForm = () => {
+  const ref = useRef<HTMLFormElement>(null);
+  const fileInputRef = useRef<HTMLInputElement>(null);
   const { user } = useUser();
 
+  const [preview, setPreview] = useState<string | null>(null);
+
+  const handleImageChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const file = event.target.files?.[0];
+    if (file) {
+      setPreview(URL.createObjectURL(file));
+    }
+  };
+
+  const handlePostAction = async (formData: FormData) => {
+    const formDataCopy = formData;
+    ref.current?.reset();
+
+    const text = formDataCopy.get("postInput") as string;
+
+    if(!text.trim()){
+      throw new Error("You must provide a post input");
+    }
+    setPreview(null)
+
+    try {
+      await createPostAction(formDataCopy);
+    } catch (error) {
+      console.log("Error creating post", error)
+    }
+
+  }
+  
+
   return (
-    <div>
-      <form action="">
+    <div className="mb-2">
+      <form ref={ref} className="p-3 bg-white rounded-lg" action={formData => {
+        //Handle form submission with server action
+
+
+        //Toast notification based on the promise above
+
+
+      }}>
         <div className="flex items-center space-x-2">
           <Avatar>
             <AvatarImage src={user?.imageUrl} />
@@ -27,23 +65,47 @@ const PostForm = () => {
             className="flex-1 outline-none rounded-full py-3 px-4 border"
           />
           {/* Only image will be picked because of accept="image/*" */}
-          <input type="file" name="image" accept="image/*" hidden />
+          <input
+            ref={fileInputRef}
+            type="file"
+            name="image"
+            accept="image/*"
+            hidden
+            onChange={handleImageChange}
+          />
           <button type="submit" hidden>
             Post
           </button>
         </div>
 
         {/* Preview Conditional Check */}
+        {preview && (
+          <div className="mt-3">
+            <img src={preview} alt="Preview" className="w-full object-cover" />
+          </div>
+        )}
 
-        <div className="">
-            <Button>
-                <ImageIcon className="mr-2" size={26} color="currentColor" />
-                Add
+        <div className="flex justify-end mt-2 space-x-2">
+          <Button type="button" onClick={() => fileInputRef.current?.click()}>
+            <ImageIcon className="mr-2" size={26} color="currentColor" />
+            {preview ? "Change" : "Add"} image
+          </Button>
+
+          {/* Add a remove preview button */}
+          {preview && (
+            <Button
+              variant="outline"
+              type="button"
+              onClick={() => setPreview(null)}
+            >
+              <XIcon className="mr-2" size={16} color="currentColor" />
+              Remove image
             </Button>
+          )}
         </div>
-
-        {/* Add a remove preview button */}
       </form>
+
+      <hr className="mt-2 border-gray-300" />
     </div>
   );
 };
---
+++ mongodb/db.ts
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+
+const connectionString = process.env.MONGODB_URI;
+
+if (!connectionString) {
+  throw new Error("Please provide a valid connection string.");
+}
+
+const connectDB = async () => {
+  if (mongoose.connection?.readyState) {
+    // console.log("---------- Already connected to MongoDB ----------");
+    return;
+  }
+
+  try {
+    await mongoose.connect(connectionString);
+  } catch (error) {
+    console.log("Error connecting to mongoDB", error);
+  }
+};
